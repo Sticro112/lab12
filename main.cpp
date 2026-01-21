14c8923eb1a22419c36f5a7d476a80a9f3cf2a7b
Sticro112 <104106470+Sticro112@users.noreply.github.com>
2026-01-21 22:02:44 +0200

    Refactor sorting and merging of NumberList instances
    Updated sorting logic and merging process for NumberList.

--- main.cpp
+++ main.cpp
@@ -4,32 +4,34 @@
 #include "NumberList.h"
 
 int main() {
+
     std::vector<NumberList> lists = {
         NumberList({1, 2, 3}),
         NumberList({10}),
         NumberList({4, 5})
     };
 
-    std::cout << "Initial lists:\n";
+
     for (const auto& l : lists)
         l.print();
 
     std::sort(lists.begin(), lists.end(),
               [](const NumberList& a, const NumberList& b) {
-                  return b > a;
+                  return !(a > b);
               });
 
-    std::cout << "\nSorted lists:\n";
     for (const auto& l : lists)
         l.print();
+
 
     NumberList& minList = lists.front();
     NumberList& maxList = lists.back();
 
-    for (int i = 0; i < minList.sum(); ++i)
-        maxList += 1;
 
-    std::cout << "\nAfter merging:\n";
+    for (int x : minList.getData()) {
+        maxList += x;
+    }
+
     for (const auto& l : lists)
         l.print();
 
